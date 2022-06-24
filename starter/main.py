101952592c4296e72cb0971a6c161dba117c8d0b
simon.kallmaier <simon.kallmaier@check24.de>
2022-06-24 16:19:54 +0200

    Add fast api base structure

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,46 @@
 # Put the code for your API here.
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from starter.ml.model import inference
+
+app = FastAPI()
+
+
+class Data(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
+
+@app.get("/")
+async def say_hello():
+    return {"greeting": "Hello!"}
+
+
+
+@app.post("/model")
+async def exercise_function(path: int, query: int, body: Value):
+    return {"path": path, "query": query, "body": body}
+
+
+@app.post("/predict", response_model=Data, status_code=200)
+def get_prediction(payload: Data):
+
+
+    prediction = inference(ticker)
+
+    response_object = {"prediction": convert(prediction_list)}
+    return response_object
---
+++ starter/test_api.py
@@ -0,0 +1,31 @@
+import json
+from fastapi.testclient import TestClient
+
+from bar import app
+
+client = TestClient(app)
+
+
+def test_post():
+    data = json.dumps({"value": 10})
+    r = client.post("/42?query=5", data=data)
+    print(r.json())
+    assert r.json()["path"] == 42
+    assert r.json()["query"] == 5
+    assert r.json()["body"] == {"value": 10}
+
+def test_get_path():
+    r = client.get("/items/42")
+    assert r.status_code == 200
+    assert r.json() == {"fetch": "Fetched 1 of 42"}
+
+
+def test_get_path_query():
+    r = client.get("/items/42?count=5")
+    assert r.status_code == 200
+    assert r.json() == {"fetch": "Fetched 5 of 42"}
+
+
+def test_get_malformed():
+    r = client.get("/items")
+    assert r.status_code != 200
